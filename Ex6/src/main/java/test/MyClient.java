8fedb5d8d383315f40580bf6826c7099ec8e625d
etinka <etinegev@gmail.com>
2018-05-26 09:40:03 +0300

    Ex6 - With xml unmarshalling

--- Ex6/src/main/java/test/MyClient.java
+++ Ex6/src/main/java/test/MyClient.java
@@ -3,6 +3,7 @@ package test;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import java.io.*;
 import java.net.Socket;
 
@@ -17,13 +18,14 @@ public class MyClient {
         socket.close();
     }
 
-    private static String jaxbObjectToXML(Level level) {
+    public static String jaxbObjectToXML(Level level) {
         String xmlString = "";
         try {
             JAXBContext context = JAXBContext.newInstance(Level.class);
             Marshaller m = context.createMarshaller();
 
             m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE); // To format XML
+            // m.marshal(level, System.out);
 
             StringWriter sw = new StringWriter();
             m.marshal(level, sw);
@@ -34,6 +36,19 @@ public class MyClient {
         }
 
         return xmlString;
+    }
+
+    public static Level convertStringToLevel(String xml) {
+        Level level = null;
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Level.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            StringReader reader = new StringReader(xml);
+            level = (Level) unmarshaller.unmarshal(reader);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return level;
     }
 
     private void readInputAndSend(BufferedReader in, PrintWriter out, String exitStr) throws IOException {
--- Ex6/src/test/java/test/MyClientTest.java
+++ Ex6/src/test/java/test/MyClientTest.java
@@ -7,6 +7,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -28,7 +29,24 @@ public class MyClientTest {
         };
 
         Assert.assertTrue("Message sent successfully", text.sendTo("localhost", 1234));
-        Assert.assertEquals(testString, byteArrayOutputStream.toString());
+        assertEquals(testString, byteArrayOutputStream.toString());
+    }
+
+    @Test
+    public void testXmlConvert() {
+        Level l = new Level();
+        l.setCx(15);
+        l.setCy(23);
+        char[][] data = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+        l.setData(data);
+
+        String xml = MyClient.jaxbObjectToXML(l);
+
+        Level l2 = MyClient.convertStringToLevel(xml);
+        assertEquals(15, l2.getCx());
+        assertEquals(23, l2.getCy());
+        assertEquals(data, l2.getData());
+
     }
 
 }
